4a7974894d8bdea088ce9f7454eb5dd3d7cb38ed
ester-l <ester.limabar@gmail.com>
2026-04-17 10:01:04 -0300

    Eu

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const MONGO_URI =
+  'mongodb+srv://esterlimabar_db_user:3ydC45CgeKI3q9YO @cadastro.kmhwd8x.mongodb.net/cadastro?appName=cadastro';
+
+async function connectDatabase() {
+  await mongoose.connect(MONGO_URI);
+  console.log('MongoDB conectado com sucesso.');
+}
+
+module.exports = connectDatabase;
